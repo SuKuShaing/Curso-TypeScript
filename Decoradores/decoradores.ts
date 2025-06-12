d3e1af0a25b1e819a5672c7aa45278f9851deee2
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-06-12 18:58:54 -0400

    Decoradores en Ts

---
+++ Decoradores/decoradores.ts
@@ -0,0 +1,28 @@
+function PersonaDec(target: Function) { // esta función debe ir antes del decorar que la llame
+    console.log(target);
+
+    target.prototype.despedir = function (despedida: string) {
+        return `${despedida}, ${this.nombre}`;
+    }
+}
+
+@PersonaDec
+class Persona2 {
+    nombre: string;
+    edad: number;
+
+    constructor(nombre: string, edad: number) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    saludar(saludo: string): string {
+        return `${saludo}, mi nombre es ${this.nombre} y tengo ${this.edad} años`;
+    }
+
+    despedir!: (despedida: string) => string; // esto es para que typescript sepa que el método despedir existe
+}
+
+let usuario = new Persona2("Juan", 20);
+console.log(usuario.saludar("Hola"));
+console.log(usuario.despedir("Adios"));
